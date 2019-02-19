88964683ad607bfdfbbcb9f251729e176413c4f3
foxalb <43451007+foxalb@users.noreply.github.com>
2019-02-19 09:01:36 -0500

    final 2

--- Tienda_Angular2_ReactJS/TiendaAngular2/karma.conf.js
+++ Tienda_Angular2_ReactJS/TiendaAngular2/karma.conf.js
@@ -1,10 +1,10 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html 
+// https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
+    frameworks: ['jasmine', 'angular-cli'], 
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
